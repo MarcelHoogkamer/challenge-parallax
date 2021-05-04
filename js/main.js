d5f7c626f25aff070fd29f238127f629d7c1b1f9
Mark <mark.hoogkamer@gmail.com>
2021-05-04 16:28:44 +0200

    More items added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var objImage= null;
 function init(){
-  objImage=document.getElementById("bird");
+  objImage=document.getElementById("birdimg");
+  objImage2=document.getElementById("balloon");
   objImage.style.position='relative';
   objImage.style.left='200px';
   objImage.style.top='200px';
@@ -30,7 +31,15 @@ function getKeyAndMove(e){
     case 'Backspace':
       stealthMode();
       break;
+    case 'Escape':
+      balloonDrop();
+      break;
   }
+}
+
+function balloonDrop(){
+  objImage2.style.top=parseInt(objImage.style.top)+50 +'px';
+  objImage2.style.top=parseInt(objImage.style.top)-50 +'px';
 }
 
 function stealthMode(){
@@ -41,23 +50,28 @@ function makeDove(){
   objImage.style.filter= 'invert(100%)';
 }
 function downCrash(){
-  objImage.style.top=parseInt(objImage.style.top)+1200 +'px';
+  objImage.style.top=parseInt(objImage.style.top)+1000 +'px';
 }
 function moveLeft(){
+  objImage.style.transform= "scaleX(-1)";
   objImage.style.left=parseInt(objImage.style.left)-5 +'px';
 }
 function moveUp(){
+  objImage.style.transform= "rotate(-90deg)";
   objImage.style.top=parseInt(objImage.style.top)-5 +'px';
 }
 function moveRight(){
+  objImage.style.transform= "scaleX(1)";
   objImage.style.left=parseInt(objImage.style.left)+5 +'px';
 }
 function moveDown(){
+  objImage.style.transform= "rotate(90deg)";
   objImage.style.top=parseInt(objImage.style.top)+5 +'px';
 }
 window.onload=init;
 
 window.addEventListener("keydown", e => {
   getKeyAndMove(e);
+  clear;
 }
 )
